82e408145c48fdd1dcc3fc3353f640a52caef4b8
Nikolay <n_hristov007@abv.bg>
2020-06-06 22:57:41 +0300

    add

---
+++ kosmonaft.vue
@@ -0,0 +1,14 @@
+// <template>
+//   <div>
+//     <p>kosmonavt</p>
+//   </div>
+// </template>
+
+// <script>
+// export default kosmonaft {
+//   name: "kosmonaft"
+// };
+// </script>
+
+// <style>
+// </style>
